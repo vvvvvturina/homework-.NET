2e8c8729a2ddea4866e4834cdee3f810a110cb16
vvvvvturina <tyurina.vladislava@gmail.com>
2021-09-28 22:39:47 +0300

    first commit

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using calculator;
+using UnitTests;
+using NUnit
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionTest()
+        {
+            //arrange
+            int a = 4;
+            int b = 5;
+            var operation = Calculator.operation.Addition;
+            //act
+            
+            //assert
+            Assert.AreEqual(8, (a+b));
+        }
+    }
+}
---
+++ calculator/Calculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace calculator
+{
+    public class Calculator
+    {
+        
+        public enum operation
+        {
+            Addition,
+            Substraction,
+            Multiplication,
+            Division,
+            NotOperator
+        }
+        
+        public static Exception NotAnOperatorException = new Exception("The operator doesn't exist.");
+        public static Exception CanNotDivideByZero = new Exception("Can not divide by zero.");
+
+        public static operation GetOperator(string argument)
+        {
+            operation operationType = operation.NotOperator;
+            switch (argument)
+            {
+                case "+":
+                    operationType = operation.Addition;
+                    break;
+                case "-":
+                    operationType = operation.Substraction;
+                    break;
+                case "*":
+                    operationType = operation.Multiplication;
+                    break;
+                case "/":
+                    operationType = operation.Division;
+                    break;
+            }
+            return operationType;
+        }
+        
+        public static double Calculate(int a, int b, operation operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case operation.Addition:
+                    result = a + b;
+                    break;
+                case operation.Division:
+                    if (b==0)
+                    {
+                        throw CanNotDivideByZero;
+                    }
+                    else
+                    {
+                        result = a / b;
+                    }
+                    break;
+                case operation.Multiplication:
+                    result = a * b;
+                    break;
+                case operation.Substraction:
+                    result = a - b;
+                    break;
+                case operation.NotOperator:
+                    throw NotAnOperatorException;
+            }
+            return result;
+        }
+    }
+}
